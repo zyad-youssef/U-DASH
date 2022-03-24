cd82cff292cfd85e3388a19ebb12579f2e8b4811
zyad-youssef <zyadibrahimyoussef@gmail.com>
2022-03-24 00:25:21 -0600

    switched from bootstrap to MUI

--- u-dash-ui/src/App.js
+++ u-dash-ui/src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Message from './components/Messages';
+import { makeStyles, IconButton, Typography, CssBaseline, AppBar, Toolbar, Container, Button, Menu as MenuIcon } from '@material-ui/core';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Message/>
+
+      {/* <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6">
+            U-Dash
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div> <Container maxWidth="sm">
+          <Typography variant="h7" allign="center" color="textPrimary" gutterBottom>
+            TEST TEXT
+          </Typography>
+        </Container></div>
+      </main>
+      <Message /> */}
+      <Header></Header>  
     </div>
   );
 }
---
+++ u-dash-ui/src/components/Header.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    boxShadow: "none",
+    backgroundColor: "#B32020"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <FormGroup>
+
+      </FormGroup>
+      <AppBar className={classes.root} position="static" >
+        <Toolbar>
+
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleMenu}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            U-Dash
+          </Typography>
+          <div>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}> Orders </MenuItem>
+              <MenuItem onClick={handleClose}> About </MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
