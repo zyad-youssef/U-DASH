2a6aec54ad0042d49f8b268cbd5d1fab85fddb31
zyad-youssef <zyadibrahimyoussef@gmail.com>
2022-08-25 19:05:22 -0600

    adding UI code

---
+++ u-dash-ui/build/precache-manifest.13e7ad19e4527d0d3b5e0d089cf71d57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aee2ccf2a7d11c6a253",
+    "url": "/static/css/main.5399187a.chunk.css"
+  },
+  {
+    "revision": "0aee2ccf2a7d11c6a253",
+    "url": "/static/js/main.74e410f3.chunk.js"
+  },
+  {
+    "revision": "39bb6501442b7e61ce8d",
+    "url": "/static/js/runtime~main.d3b876c7.js"
+  },
+  {
+    "revision": "f02aec9540a747a1d33a",
+    "url": "/static/js/2.bc6d262d.chunk.js"
+  },
+  {
+    "revision": "8df7ebfe210f7e4ea621",
+    "url": "/static/js/3.5f7dd33c.chunk.js"
+  },
+  {
+    "revision": "e57626440a25f8ee103b041bdda45b70",
+    "url": "/static/media/ulogo.e5762644.png"
+  },
+  {
+    "revision": "b6d6a63779aefb10595fe991e59ec505",
+    "url": "/static/media/Dasher.b6d6a637.jpg"
+  },
+  {
+    "revision": "76b00cda548c46d7ce923277536c6d44",
+    "url": "/index.html"
+  }
+];
---
+++ u-dash-ui/build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.13e7ad19e4527d0d3b5e0d089cf71d57.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
